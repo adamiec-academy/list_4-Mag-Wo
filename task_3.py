5b285048349eae3e53d05fc101f7c3b11e4dbcda
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2023-01-02 14:41:50 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,12 @@
 def min_max(data):
-    return 0, 0
+    
+    min = data[0]
+    max = data[0]
+    
+    for value in data:
+        if value > max:
+            max = value
+        elif value < min:
+            min = value
+            
+    return min, max
