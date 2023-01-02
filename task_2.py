6ab9f63f37f647de90d846e889431182db33de9b
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2023-01-02 14:38:35 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,16 @@
 def my_split(text):
-    return [text]
+
+    list = []
+    word = ''
+
+    for letter in text:
+        if letter != " ":
+           word += letter
+        elif word != "":
+            list.append(word)
+            word = ""
+
+    if word != "":
+        list.append(word)
+    
+    return list
